7ad7b39b6f6d658a4e8ef65d19887a0c5a69c1c9
Phillip Langland <philliplangland@gmail.com>
2018-11-03 20:31:07 -0500

    created test data to test server routes

--- server/src/routes/list.js
+++ server/src/routes/list.js
@@ -1,3 +1,17 @@
 // list route
 // will recieve all RESTful requests that deal with the list
 
+import { Router } from 'express';
+
+//test data
+let list = [
+    {
+        id: 1,
+        task: "go to store"
+    },
+    {
+        id: 2,
+        task: "do dishes"
+    }
+];
+
