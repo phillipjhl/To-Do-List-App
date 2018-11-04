df6d4577341b4005336b4c0a281e9db0dd4d8105
Phillip Langland <philliplangland@gmail.com>
2018-11-03 20:25:18 -0500

    index.js router functionality- will point to listRouter

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,2 +1,15 @@
 //index.js - holds all the routes for the server
 //all routes should be imported to here
+
+
+import { Router } from 'express';
+import listRouter from './list';
+
+//create router to point to files
+let router = Router();
+
+//list router- will hold all the REST request for API
+//url: '/api/list'
+router.use('/list', listRouter);
+
+export default router;
