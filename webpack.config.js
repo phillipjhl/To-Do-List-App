6fe69b7dfe41a68ef2649c4c35c71ac151c3eb9a
Phillip Langland <philliplangland@gmail.com>
2018-11-03 21:33:52 -0500

    set bundling input and ouput, need to do this before writing scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,3 +2,11 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const CLIENT_DEST = path.join(__dirname, './client/dist');
+
+module.exports = {
+    //entry point for babel to compile files
+    entry: ['@babel/polyfill', './client/src/index.js'],
+    //output directory of new bundle.js file
+    output: { path: CLIENT_DEST, filename: 'bundle.js' },
+    module: {}
+};
