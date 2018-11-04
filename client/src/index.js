d82a708dca8f164ebec6005398d0bdfb8172ab3f
Phillip Langland <philliplangland@gmail.com>
2018-11-03 21:18:41 -0500

    entrypoint for entire front-end, single render component for whole app

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+//single ReactDOM.render to render App component which
+//will conatin the main navigation for whole application
+//Renders with the 'root' div in index.html
+ReactDOM.render(<App />, document.getElementById('root'));
