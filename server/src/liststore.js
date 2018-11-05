c03bc2095b33586186b22156777efb27c379e01a
Phillip Langland <philliplangland@gmail.com>
2018-11-04 20:08:38 -0600

    added func to use object assign to return copy of file, test w/ postman

--- server/src/liststore.js
+++ server/src/liststore.js
@@ -2,36 +2,41 @@
  * Service file to read and write to a JSON file to store list data.
  */
 
- import fs from 'fs';
- let toDoList = { nextid: 0 };
+import fs from 'fs';
+let toDoList = { nextid: 0 };
 
- //conditional to check id there is already json file
- //set contents to toDoList variable
- if (fs.exists('list.json')) {
-     toDoLst = JSON.parse(fs.readFile('toDoList.json'));
- }
+//conditional to check id there is already json file
+//set contents to toDoList variable
+if (fs.exists('list.json')) {
+    toDoLst = JSON.parse(fs.readFile('toDoList.json'));
+}
 
-
-//get all function
+//async get all function
+//returns copy object(to do list)
+let getList = async () => {
+    let copy = await Object.assign({}, toDoList);
+    return copy;
+};
 
 //post function
+
 //will create a task item and increments id
 let createTask = async (task) => {
     toDoList[toDoList.nextid++] = task;
     await writeList();
 }
 
- //put function
+//put function
 
- //delete function
+//delete function
 
- //write to file- helper function
- let writeList = async () => {
-        await fs.writeFileSync('toDoList.json', JSON.stringify(toDoList));
- }
+//write to file- helper function
+let writeList = async () => {
+    await fs.writeFileSync('toDoList.json', JSON.stringify(toDoList));
+}
 
- //export
- export default {
-     getList,
-     createTask
- }
+//export
+export default {
+    getList,
+    createTask
+}
