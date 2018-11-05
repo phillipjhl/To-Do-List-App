46345d4cd11abf5e2bee73a7ef0e00d12d173c2c
Phillip Langland <philliplangland@gmail.com>
2018-11-04 20:24:26 -0600

    added ifElse block to determine if there are route params

--- server/src/routes/list.js
+++ server/src/routes/list.js
@@ -20,22 +20,26 @@ let router = Router();
 
 router.get('/:id?', (req, res, next) => {
     let id = req.params.id;
-    listStore.getList()
-    .then(list => {
-        res.json(list);
-    })
-    //catch error and pass it to next middleware, reduces uncaught exceptions
-    .catch(next);
+    if (id) {
+
+    } else {
+        listStore.getList()
+            .then(list => {
+                res.json(list);
+            })
+            //catch error and pass it to next middleware, reduces uncaught exceptions
+            .catch(next);
+    }
 });
 
 //post request for '/api/list
 router.post('/', (req, res, next) => {
     listStore.createTask(req.body)
-    .then(success => {
-        res.sendStatus(201);
-    })
-    //catch error and pass it to next middleware, reduces uncaught exceptions
-    .catch(next);
+        .then(success => {
+            res.sendStatus(201);
+        })
+        //catch error and pass it to next middleware, reduces uncaught exceptions
+        .catch(next);
 })
 
 export default router;
