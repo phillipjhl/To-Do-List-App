2e82e975b534c0cae539d106b485e881b737712b
Phillip Langland <philliplangland@gmail.com>
2018-11-05 15:05:20 -0600

    test content for listpage

---
+++ client/src/components/ListPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ListPage extends Component {
+
+    render() {
+        return (
+            <h1>List Page</h1>
+        );
+    }
+
+}
