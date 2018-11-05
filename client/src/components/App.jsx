b6ca5f69636f811185ec9149450b4b14c14d9332
Phillip Langland <philliplangland@gmail.com>
2018-11-05 14:07:06 -0600

    main router working with simple homepage component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
+import HomePage from './HomePage';
+
 class Navigation extends Component {
 
     render() {
---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class HomePage extends Component {
+    
+
+    render() {
+        return (
+            <h1 className="text-primary">Hello there.</h1>
+        );
+    }
+}
