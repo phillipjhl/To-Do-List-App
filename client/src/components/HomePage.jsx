f0b9540b062a80b887bfc6f315deb387246fe1b3
Phillip Langland <philliplangland@gmail.com>
2018-11-05 14:59:23 -0600

    starting homepage component - landing page of app

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 export default class HomePage extends Component {
-    
+
 
     render() {
         return (
-            <h1 className="text-primary">Hello there.</h1>
+            <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+                <main role="main" class="inner cover">
+                    <h1 class="cover-heading">Welcome to Your New Way of Doing!</h1>
+                    <p class="lead">Create Tasks and Get Them Them Done</p>
+                    <p class="lead">
+                        {/* will be Link to listPage */}
+                        <a href="#" class="btn btn-lg btn-secondary">Let's Get Started</a>
+                    </p>
+                </main>
+            </div>
         );
     }
 }
