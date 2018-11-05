c50d3c2a91ff43fdff9f4b3b1420a2aada7ec267
Phillip Langland <philliplangland@gmail.com>
2018-11-04 20:10:27 -0600

    added promise chain for get/post requests, tested with postman

--- server/src/routes/list.js
+++ server/src/routes/list.js
@@ -2,28 +2,37 @@
 // will recieve all RESTful requests that deal with the list
 
 import { Router } from 'express';
+import listStore from '../liststore';
 
 //test data
-let list = [
-    {
-        id: 1,
-        task: "go to store"
-    },
-    {
-        id: 2,
-        task: "do dishes"
-    }
-];
+// let list = [
+//     {
+//         id: 1,
+//         task: "go to store"
+//     },
+//     {
+//         id: 2,
+//         task: "do dishes"
+//     }
+// ];
 
 let router = Router();
 
 //route works
-router.get('/', (req, res) => {
-    try {
+router.get('/', (req, res, next) => {
+    listStore.getList()
+    .then(list => {
         res.json(list);
-    } catch (e) {
-        res.sendStatus(404);
-    }
+    })
+    .catch(next);
 });
+
+router.post('/', (req, res, next) => {
+    listStore.createTask(req.body)
+    .then(success => {
+        res.sendStatus(201);
+    })
+    .catch(next);
+})
 
 export default router;
