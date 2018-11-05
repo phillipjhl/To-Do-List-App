7daa591c0381214b692d13a62a4123f0870b4b12
Phillip Langland <philliplangland@gmail.com>
2018-11-05 13:59:15 -0600

    added import for router navigation

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 class Navigation extends Component {
 
     render() {
         return (
-            <div className="text-primary">
-                hello there
-            </div>
+            
         );
     }
 }
