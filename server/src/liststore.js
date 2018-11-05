c4997df4c12bb745bbffea04e7ce1dc4ff92cef4
Phillip Langland <philliplangland@gmail.com>
2018-11-04 18:55:48 -0600

    file layout

--- server/src/liststore.js
+++ server/src/liststore.js
@@ -2,5 +2,19 @@
  * Service file to read and write to a JSON file to store list data.
  */
 
- import { fs } from 'fs';
+ import fs from 'fs';
  let list = { nextid: 0 };
+
+ //conditional to check for json file
+
+ //get function
+
+ //get all function
+
+ //put function
+
+ //delete function
+
+ //write to file- helper function
+
+ //export
