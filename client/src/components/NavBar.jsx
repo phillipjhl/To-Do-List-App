773e7df12e8102075fded366e7f3ccb2d9a2d493
Phillip Langland <philliplangland@gmail.com>
2018-11-06 10:30:50 -0600

    added to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Link from 'react-router-dom';
 
 export default function NavBar(props) {
     return (
-        <nav class="navbar fixed-top navbar-light bg-light">
-            <a class="navbar-brand" href="#">Home</a>
+        <nav class="navbar fixed-top navbar-dark bg-dark">
+            <div className="collapse navbar-collapse" id="navbar">
+                <div>
+                <Link to="/list" class="navbar-brand">Home</Link>
+                </div>
+            </div>
+
         </nav>
     );
 }
