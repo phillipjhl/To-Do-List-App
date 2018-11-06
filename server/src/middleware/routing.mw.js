16108976db970a1d54f78a93302586479cb67ff7
Phillip Langland <philliplangland@gmail.com>
2018-11-05 20:31:53 -0600

    comments

--- server/src/middleware/routing.mw.js
+++ server/src/middleware/routing.mw.js
@@ -1,6 +1,7 @@
 import { join } from 'path';
 
 function stateRouting(req, res, next) {
+    //if going to be served by /api else send index.html for react
     if (isServerAsset(req.url)) {
         next();
     } else {
