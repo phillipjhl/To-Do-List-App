5a7f0324491d2393a1c09c5fa0481e09fda694ac
Phillip Langland <philliplangland@gmail.com>
2018-11-05 21:06:10 -0600

    added test nav bar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NavBar(props) {
+    return (
+        <nav class="navbar fixed-top navbar-light bg-light">
+            <a class="navbar-brand" href="#">Home</a>
+        </nav>
+    );
+}
