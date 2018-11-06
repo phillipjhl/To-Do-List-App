9d0f6bc1a0bfa9c4f182daa8fe1b8e75af7364b7
Phillip Langland <philliplangland@gmail.com>
2018-11-06 10:42:33 -0600

    needs to be className

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -5,13 +5,13 @@ export default class HomePage extends Component {
 
     render() {
         return (
-            <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column mt-auto mb-auto text-center">
-                <main role="main" class="inner cover">
-                    <h1 class="cover-heading">Welcome to Your New Way of Doing!</h1>
-                    <p class="lead">Create Tasks and Get Them Them Done</p>
-                    <p class="lead">
+            <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column mt-auto mb-auto text-center">
+                <main role="main" className="inner cover">
+                    <h1 className="cover-heading">Welcome to Your New Way of Doing!</h1>
+                    <p className="lead">Create Tasks and Get Them Them Done</p>
+                    <p className="lead">
                         {/* will be Link to listPage */}
-                        <a href="#" class="btn btn-lg btn-secondary">Let's Get Started</a>
+                        <a href="#" className="btn btn-lg btn-secondary">Let's Get Started</a>
                     </p>
                 </main>
             </div>
