b524fb526853bc09347605a86230d8e9a2a2d220
Phillip Langland <philliplangland@gmail.com>
2018-11-06 17:59:45 -0600

    making tasks into card bootstrap components

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -8,14 +8,22 @@ class Task extends Component {
         super(props);
         this.state = {
             //setting passed prop object into state
-            task: this.props.data
+            task: this.props.data,
+            isCompleted: false,
+            isHovered: false
         }
     }
 
     render() {
         console.log(this.state);
         return (
-            <h1>Text {this.props.data.id}: {this.props.data.text}</h1>
+            <article className="card border-primary">
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.task.text}</h5>
+                    <h6 className="card-subtitle mb-2">{this.state.task.location}</h6>
+                    <span className="card-text">{this.state.task.time}</span>
+                </div>
+            </article>
         );
     }
 }
