413019f95ddc9dd0c40b92ede41eea1eaf110eb7
Phillip Langland <philliplangland@gmail.com>
2018-11-06 12:15:17 -0600

    basic jumbotron for now

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -1,1 +1,18 @@
 // Jumbotron component
+
+import React from 'react';
+
+function Jumbotron(props) {
+    return (
+        <div className="jumbotron jumbotron-fluid bg-dark text-white">
+            <div className="container">
+                <div className="col-sm-4 inner-cover">
+                    <h6>YOUR</h6>
+                    <h6>TASKS</h6>                   
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
