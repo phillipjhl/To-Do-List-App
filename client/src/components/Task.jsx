5b884fbe52dd4304f52355be1130e6ecbe4ef3cf
Phillip Langland <philliplangland@gmail.com>
2018-11-06 17:00:14 -0600

    setting passed props into state so it can be changed

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -6,10 +6,14 @@ import { Link } from 'react-router-dom';
 class Task extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            //setting passed prop object into state
+            task: this.props.data
+        }
     }
 
     render() {
+        console.log(this.state);
         return (
             <h1>Text {this.props.data.id}: {this.props.data.text}</h1>
         );
