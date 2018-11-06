53a8f3cb336b5dada569f05efb782b781ab07229
Phillip Langland <philliplangland@gmail.com>
2018-11-06 11:29:02 -0600

    initiating states and commenting out componentDidMount method

--- client/src/components/ListPage.jsx
+++ client/src/components/ListPage.jsx
@@ -3,6 +3,16 @@ import React, { Component, Fragment } from 'react';
 export default class ListPage extends Component {
     contructor(props) {
         super(props);
+
+        this.state = {
+            isLoading: true,
+            list: []
+        }
+    }
+
+    componentDidMount() {
+        //make api call for list
+        //map over lsit to render list of task, set it to state
     }
 
     render() {
