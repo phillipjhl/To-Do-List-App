7aa651e0950914ba5daf4f1e11fe58569e60ad93
Phillip Langland <philliplangland@gmail.com>
2018-11-06 16:50:46 -0600

    added to task to test it with list

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,1 +1,17 @@
 //Individual Task Component
+
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+class Task extends Component {
+    contructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <h1>Text {this.props.id}: {this.props.text}</h1>
+        );
+    }
+}
