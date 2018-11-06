a743fdd73fe0c7b45cc639eb46d0500777e8d323
Phillip Langland <philliplangland@gmail.com>
2018-11-06 13:11:09 -0600

    setting up state object

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -6,11 +6,17 @@ import React, { Component, Fragment } from 'react';
 export default class ListPage extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isLoading: true,
+            list: []
+        }
     }
+
 
     render() {
         return (
-            <h1 className="text-primary">List</h1>
+            <h1 className="text-primary col-sm-9">To Do</h1>
         );
     }
 
