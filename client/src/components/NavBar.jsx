e3f04e08fb44b1d1b31fe7e31114a423cace6b9e
Phillip Langland <philliplangland@gmail.com>
2018-11-06 10:34:05 -0600

    added collapse button

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,8 +4,13 @@ import Link from 'react-router-dom';
 export default function NavBar(props) {
     return (
         <nav class="navbar fixed-top navbar-dark bg-dark">
+            
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            
             <div className="collapse navbar-collapse" id="navbar">
-                <div>
+                <div className="navbar-nav flex-row">
                 <Link to="/list" class="navbar-brand">Home</Link>
                 </div>
             </div>
