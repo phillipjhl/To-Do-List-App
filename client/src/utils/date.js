af1f0e53c935e80e316803b50a398ee5f3554a9b
Phillip Langland <philliplangland@gmail.com>
2018-11-06 12:46:11 -0600

    added to getDate func to return a string of the current date

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,7 +1,14 @@
 // Creating date string
 
 function getDate() {
-    
+    let d = new Date();
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let date = d.getDate();
+
+    let fullDate = `${months[d.getMonth()]} ${date}, ${d.getFullYear()}`
+
+    return fullDate; // Returns string
 }
 
 export default getDate;
