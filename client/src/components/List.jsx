32a310b0b5bc5f3fb19c5f6877b5955b8e9fbc38
Phillip Langland <philliplangland@gmail.com>
2018-11-07 12:37:03 -0600

    added back task counter, what I deleted on accident

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -62,6 +62,7 @@ export default class ListPage extends Component {
                 <aside className="col-md-8">
                     <h2 className="col-sm-9 text-primary">To Do</h2>
                     {this.createList()}
+                    <h6 className="col-sm-2 text-muted my-2">{this.state.toDolist.length} Tasks</h6>
                 </aside>
             </Fragment>
         );
