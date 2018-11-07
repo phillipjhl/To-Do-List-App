acbbf5eec893375818d01579d6bff2d5e9b0fd43
Phillip Langland <philliplangland@gmail.com>
2018-11-06 18:38:34 -0600

    removed navbar brand, didnt like the look

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function NavBar(props) {
     return (
         <nav className="navbar fixed-top navbar-light">
-            <Link to="/list" className="navbar-brand">Home</Link>
+            <span></span>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
