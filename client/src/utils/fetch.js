8aa9339fd1540560ce0cccebde39ffe708a62b97
Phillip Langland <philliplangland@gmail.com>
2018-11-07 13:58:19 -0600

    adding headers so each call of makeFetch will have preset headers

--- client/src/utils/fetch.js
+++ client/src/utils/fetch.js
@@ -5,5 +5,11 @@ function makeFetch(url, info) {
 }
 
 function json(url, method = 'GET', payload = {}) {
-    
+    let data = {
+        method,
+        body: JSON.stringify(payload),
+        headers: new Headers({
+            'Content-Type': 'application/json',
+        })
+    };
 }
