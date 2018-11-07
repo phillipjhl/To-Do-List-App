cfab5eb1578531d553e232f8f504cddc9ba3aebd
Phillip Langland <philliplangland@gmail.com>
2018-11-07 13:53:38 -0600

    added import for polyfilling fetch es, only have to import here

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -7,11 +7,13 @@ export default class HomePage extends Component {
     render() {
         return (
             <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column mt-auto mb-auto text-center">
+
                 <main role="main" className="inner cover">
                     <h1 className="cover-heading">Welcome to Your New Way of Doing!</h1>
                     <p className="lead">Create Tasks and Get Them Them Done</p>
-                    <Link to="/list" className="btn btn-lg" type="btn" aria-label="Let's Get Started">Let's Get Started</Link>
+                    <Link to="/list" className="btn btn-lg" type="button" aria-label="Let's Get Started">Let's Get Started</Link>
                 </main>
+
             </div>
         );
     }
