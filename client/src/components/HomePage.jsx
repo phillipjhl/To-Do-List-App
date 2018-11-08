9148bb0937b775a39b650dcd93a3e69a194b9b60
Phillip Langland <philliplangland@gmail.com>
2018-11-08 13:02:32 -0600

    added footer

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import Footer from './Footer';
+
 export default class HomePage extends Component {
 
 
@@ -8,11 +10,13 @@ export default class HomePage extends Component {
         return (
             <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column mt-auto mb-auto text-center">
 
-                <main role="main" className="inner cover">
+                <main role="main" className="my-5 py-5">
                     <h1 className="cover-heading">Welcome to Your New Way of Doing!</h1>
                     <p className="lead">Create Tasks and Get Them Them Done</p>
-                    <Link to="/list" className="btn btn-lg" type="button" aria-label="Let's Get Started">Let's Get Started</Link>
+                    <Link to="/list" className="btn btn-lg" role="button" aria-label="Let's Get Started">Let's Get Started</Link>
                 </main>
+
+                <Footer />
 
             </div>
         );
