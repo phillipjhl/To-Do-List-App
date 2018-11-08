8dba2606a01c35ad0f7217da92594f549a941d86
Phillip Langland <philliplangland@gmail.com>
2018-11-08 16:50:33 -0600

    changes to homepage and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function NavBar(props) {
     return (
-        <footer className="bd-footer text-muted mt-5 pt-5">
-            <div className="container-fluid p-3 p-md-5">
-                <p>Created by Phillip Langland</p>
+        <footer className="footer text-muted">
+            <div className="container">
+                <span className="text-muted">Created by Phillip Langland</span>
             </div>
         </footer>
     );
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
 import Footer from './Footer';
+import $ from 'jquery';
+
 
 export default class HomePage extends Component {
 
+    componentDidMount() {
+        $('#main').addClass('fade-in');
+    }
 
     render() {
         return (
-            <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column mt-auto mb-auto text-center">
+            <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column mt-auto mb-auto text-center justify-content-center">
 
-                <main role="main" className="my-5 py-5">
+                <main role="main" id="main" className="width-75 my-5 py-5">
                     <h1 className="cover-heading">Welcome to Your New Way of Doing!</h1>
-                    <p className="lead">Create Tasks and Get Them Them Done</p>
                     <Link to="/list" className="btn btn-lg" role="button" aria-label="Let's Get Started">Let's Get Started</Link>
+                    <p className="lead">Create Tasks and Get Them Them Done</p>
                 </main>
 
-                <Footer />
+            <Footer />
 
             </div>
         );
