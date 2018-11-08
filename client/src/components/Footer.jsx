e13e0363d4a3efe409a7a04fb98ca745cf705471
Phillip Langland <philliplangland@gmail.com>
2018-11-08 02:57:05 -0600

    adding footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar(props) {
+    return (
+        <footer className="bd-footer text-muted">
+            
+
+            
+
+        </footer>
+    );
+}
+
+export default NavBar;
