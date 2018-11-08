62a4cedd412c958d63f0ba8434a1ec1e344226e4
Phillip Langland <philliplangland@gmail.com>
2018-11-08 03:27:06 -0600

    changing bootstrap themes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom';
 
 function NavBar(props) {
     return (
-        <footer className="bd-footer text-muted">
-            
-
-            
-
+        <footer className="bd-footer text-muted mt-5 pt-5">
+            <div className="container-fluid p-3 p-md-5">
+                <p>Created by Phillip Langland</p>
+            </div>
         </footer>
     );
 }
