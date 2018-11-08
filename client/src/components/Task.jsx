9df9336b256387fc35afc6b90afea23cc48231c2
Phillip Langland <philliplangland@gmail.com>
2018-11-07 19:33:19 -0600

    going to make each task value a form input to change state

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -2,6 +2,8 @@
 
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 class Task extends Component {
     constructor(props) {
@@ -9,17 +11,31 @@ class Task extends Component {
         this.state = {
             //setting passed prop object into state
             task: this.props.data,
-            isCompleted: false,
-            isHovered: false
+            isCompleted: false
         }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
     }
 
     render() {
-        console.log(this.state);
+        console.log(this.state.task);
         return (
             <article className="card border-primary">
                 <div className="card-body">
-                    <h5 className="card-title">{this.state.task.text}</h5>
+                    <h5 className="card-title">
+                        {this.state.task.text}
+                        <FontAwesomeIcon icon={faEllipsisV} size="1x" className="float-right" />
+                    </h5>
                     <h6 className="card-subtitle mb-2 text-muted">{this.state.task.location}</h6>
                     <span className="card-text text-muted">{this.state.task.time}</span>
                 </div>
